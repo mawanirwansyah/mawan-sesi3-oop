d4f069098fb348f10373572ccb955fedd55aa138
mawanirwansyah <64650892+mawanirwansyah@users.noreply.github.com>
2020-08-24 21:41:15 +0700

    Upload tugas Sesi 3 OOP

---
+++ index.js
@@ -0,0 +1,42 @@
+class Movie {
+    constructor(title, duration, category){
+        this.title = title;
+        this.duration = duration;
+        this.category = category;
+    }
+    watch() {
+        return `Kamu Sedang menonton Film ${this.title} dengan kategori ${this.category} berdurasi ${this.duration}` 
+    }
+}
+
+
+class Soundtrack extends Movie {
+    constructor(title, duration, category, titleSong, singer){
+     super(title, duration, category);
+     this.titleSong = titleSong;
+     this.singer = singer;
+    }
+    movieSoundtrack() {
+        return `Film ${this.title} Memiliki Lagu ${this.titleSong} dengan penyanyi ${this.singer}` 
+    }
+}
+
+class Channel extends Movie {
+    constructor (title, duration, category, channelName){
+        super(title, duration, category);
+        this.channelName = channelName;
+    }
+    streamAt() {
+        return `Film ${this.title} ditayangkan di ${this.channelName}`
+    }
+}
+
+let movie = new Movie('Suspicious Partner','60 minutes','Romance');
+let soundtrack = new Soundtrack ('Suspicious Partner','60 minutes','Romance','How do you fee','Ridho Majid');
+let channel = new Channel ('Suspicious Partner','Netflix');
+
+console.log(movie.watch());
+console.log(soundtrack.movieSoundtrack());
+console.log(channel.streamAt());
+
+
